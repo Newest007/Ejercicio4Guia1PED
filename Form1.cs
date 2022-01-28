00e60ae6d791d5d7297704b5981b156cfe9e9b3f
Arian <adilsonarian@outlook.com>
2022-01-27 20:27:11 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio4Guía4
+{
+    public partial class Form1 : Form
+    {
+
+        enum Posicion
+        {
+            izquierda,derecha,arriba,abajo
+        }
+
+        private int x;
+        private int y;
+        private Posicion objposicion, limitex, limitey;
+
+        public Form1()
+        {
+            InitializeComponent();
+            x = 50;
+            y = 50;
+            objposicion = Posicion.abajo;
+            limitex = (Posicion)Convert.ToInt16(value: Size.Width);
+            limitey = (Posicion)Convert.ToInt16(value: Size.Height);
+        }
+
+        private void timermov_Tick(object sender, EventArgs e)
+        {
+            if (objposicion == Posicion.derecha)
+            { x += 10; }
+
+            else if (objposicion == Posicion.izquierda)
+            { x -= 10; }
+
+            else if (objposicion == Posicion.arriba)
+            { y -= 10; }
+
+            else if(objposicion == Posicion.abajo)
+            { y += 10; }
+
+            /*else if(x.Equals(limitex) || y.Equals(limitey))
+            {
+                MessageBox.Show("hola");
+            }*/
+
+
+            Invalidate();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(new Bitmap("jettValorant.jpg"), x, y, 65, 65);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                objposicion = Posicion.izquierda;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                objposicion = Posicion.derecha;
+            }
+            if(e.KeyCode == Keys.Up)
+            {
+                objposicion = Posicion.arriba;
+            }
+            if(e.KeyCode == Keys.Down)
+            {
+                objposicion = Posicion.abajo;
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
